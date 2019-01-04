f8190f5228a556fdd50209755738d6dd953ce553
justinnunez1717 <justinnunez1717@gmail.com>
2019-01-03 18:50:31 -0600

    Added basic front end using Vue

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 
 const app = express();
 
+// Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -3,7 +3,7 @@ const mongodb = require('mongodb');
 
 const router = express.Router();
 
-// Ger Posts
+// Get Posts
 router.get('/', async (req, res) => {
   const posts = await loadPostsCollection();
   res.send(await posts.find({}).toArray());
